8b88a686f08a1aec90b3a5477fd9b36e760b7747
Issam Mani <issamouu69@gmail.com>
2024-12-03 21:06:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 // Directory to save files
 const saveDir = path.join(__dirname, "data");
